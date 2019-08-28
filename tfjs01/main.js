9782e7e6964121964437a4c4ec2d335b1710093d
Liz Myers <liz@myersdesign.com>
2019-08-28 21:33:42 +0200

    fixing the links

--- tfjs01/main.js
+++ tfjs01/main.js
@@ -22,10 +22,10 @@ const NUM_CLASSES = 4;
 
 // Icons array for dynamically generated buttons
 const iconsArr = [
-    "./img/icon_play.svg",
-    "./img/icon_pause.svg",
-    "./img/icon_snd_on.svg",
-    "./img/icon_snd_off.svg"
+    "images/icon_play.svg",
+    "images/icon_pause.svg",
+    "images/icon_snd_on.svg",
+    "images/icon_snd_off.svg"
 ];
 
 // YouTube playlist (video IDs)
